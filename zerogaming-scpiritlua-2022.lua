6358bd006ba4fc2595043227b46aaf0ab808be76
ZEROHUB2022 <117346632+ZEROHUB2022@users.noreply.github.com>
2022-11-10 00:23:22 +0700

    GUI ZERO HUB

---
+++ zerogaming-scpiritlua-2022.lua
@@ -0,0 +1,11 @@
+repeat wait() until game:IsLoaded()
+local PlaceId = game.PlaceId
+if PlaceId == 2753915549 or PlaceId == 4442272183 or PlaceId == 7449423635 then
+    if _G.Beta then
+        loadstring(game:HttpGet"https://raw.githubusercontent.com/ZEROHUB2022/ZEROHUB2022/main/LOADING-GUI-V0.2README.md")()
+        return
+    end
+    loadstring(game:HttpGet"https://raw.githubusercontent.com/ZEROHUB2022/ZEROHUB2022/main/LOADING-GUI-V0.2README.md")()
+elseif PlaceId == 2619619496 then
+    loadstring(game:HttpGet"https://raw.githubusercontent.com/acsu123/GAME/main/bedwar.txt")()
+end
